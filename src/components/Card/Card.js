cc223c852fb3dd82440fcaf1627d35a83e65fd81
max77p <shanindrakumar@gmail.com>
2018-08-29 23:05:40 -0400

    update

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -7,12 +7,13 @@ const Cardfunc = () => {
   return (
     <Card>
       <div className="gameTitle">
-      <img src={logo} className="App-logo" alt="logo"/>
+      <div class="logos">
         <h1>Clicky Game!</h1>
         <h5>
           Click on an image to earn points, but don't click on any more than
           once!
         </h5>
+        </div>
       </div>
     </Card>
   );
